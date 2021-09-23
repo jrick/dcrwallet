c0e343dc754441c50f97e1aaad81dc2d05a8d9c7
Josh Rickmar <jrick@companyzero.com>
2021-09-23 15:22:54 +0000

    go get for installing exes is deprecated, use go install

--- mobile/gen.go
+++ mobile/gen.go
@@ -1,4 +1,4 @@
 package mobile
 
-//go:generate go get golang.org/x/mobile/cmd/gobind
-//go:generate go get golang.org/x/mobile/cmd/gomobile
+//go:generate go install golang.org/x/mobile/cmd/gobind
+//go:generate go install golang.org/x/mobile/cmd/gomobile
